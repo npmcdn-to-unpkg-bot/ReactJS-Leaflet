c057592e0968890e5334f8dffc863ad15e7083c1
jamunoz <munozja@outlook.com>
2016-07-19 02:59:45 -0400

    Finally, Appbar and Drawer are working!

--- app.jsx
+++ app.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+//import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import AppBar from 'material-ui/AppBar';
-import LeftDrawer from './LeftDrawer.jsx';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 //import Saludo from './Saludo.jsx';
 
 
@@ -14,18 +16,24 @@ class App extends React.Component {
 		this.state = {open: false};
 	}
 
-	handleTouchMap() {
-		this.setState({open: !this.state.open});
-	}
+	handleTouchMap = () => this.setState({open: !this.state.open});
 
 	render() {
 		return (
 			<MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-				<AppBar
-					title = { "Test 1" }
-					onLeftIconButtonTouchTap={this.handleTouchMap.bind(this)}
-				/>
-				<LeftDrawer open={this.state.open} />
+				<div>
+					<AppBar
+						title="Test Bar"
+						onLeftIconButtonTouchTap={this.handleTouchMap}
+					/>
+					<Drawer open={this.state.open}>
+						<AppBar title="Test Bar" 
+							onLeftIconButtonTouchTap={this.handleTouchMap}
+						/>
+						<MenuItem>Menu Item 1</MenuItem>
+						<MenuItem>Menu Item 2</MenuItem>
+					</Drawer>
+				</div>
 			</MuiThemeProvider>  
 		);
 	}
